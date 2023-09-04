82a18fb60ed50f4c79ea33b39a9d8db7d17488a0
geoffroy adnet <comateen_1@hotmail.com>
2023-09-04 20:23:05 +0200

    test unitaire pour vérifier le bonne fonctionne ment de la méthode UpdateQuality

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -6,13 +6,82 @@ namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        /// <summary>
+        /// Méthode de test de l'item, son prix de vente, sa qualité, son prix de vente attendu à la sortie, et sa qualité à la sortie
+        /// </summary>
+        /// <param name="itemName"></param>
+        /// <param name="sellIn"></param>
+        /// <param name="quality"></param>
+        /// <param name="expectedSellIn"></param>
+        /// <param name="expectedQuality"></param>
+        private void TestUpdateQuality(string itemName, int sellIn, int quality, int expectedSellIn, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                {
+                    Name = itemName,
+                    SellIn = sellIn,
+                    Quality = quality,
+                }
+            };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(expectedQuality, Items[0].Quality);
+            Assert.Equal(expectedSellIn, Items[0].SellIn);
+        }
+
+        [Theory]
+        [InlineData(20, 1, 19, 0)]
+        [InlineData(11, 2, 10, 1)]
+        [InlineData(6, 0, 5, 0)]
+        [InlineData(0, 0, -1, 0)]
+        public void TestStandardItem(int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            TestUpdateQuality("Standard", sellIn, quality, expectedSellIn, expectedQuality);
+        }
+
+        
+
+        [Theory]
+        [InlineData(20, 1, 19, 2)]
+        [InlineData(11, 3, 10, 4)]
+        [InlineData(10, 2, 9, 4)]
+        [InlineData(5, 3, 4, 6)]
+        [InlineData(0, 10, -1, 0)]
+        public void TestBackstage(int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            TestUpdateQuality("Backstage passes to a TAFKAL80ETC concert", sellIn, quality, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 0, 2)]
+        [InlineData(0, 2, -1, 4)]
+        [InlineData(-1, 3, -2, 5)]
+        [InlineData(0, 0, -1, 2)]
+        public void TestAgedBrie(int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            TestUpdateQuality("Aged Brie", sellIn, quality, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 0, 0)]
+        [InlineData(0, 0, -1, 0)]
+        [InlineData(0, 2, -1, 0)]
+        [InlineData(1, 3, 0, 2)]
+        [InlineData(2, 2, 1, 1)]
+        public void TestConjuredItem(int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            TestUpdateQuality("Conjured Mana Cake", sellIn, quality, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, 80, 1, 80)]
+        [InlineData(2, 80, 2, 80)]
+        public void TestSulfuras(int sellIn, int quality, int expectedSellIn, int expectedQuality)
+        {
+            TestUpdateQuality("Sulfuras, Hand of Ragnaros", sellIn, quality, expectedSellIn, expectedQuality);
         }
     }
 }
+
