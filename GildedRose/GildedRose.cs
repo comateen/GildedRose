d05a528ae3cd7673999716e26e3127298ba43247
geoffroy adnet <comateen_1@hotmail.com>
2023-09-04 21:22:11 +0200

    je fusionne le if imbriqué avec sufuras

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -14,18 +14,15 @@ namespace GildedRoseKata
         {
             foreach (Item item in Items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    if (item.Quality > 0)
                     {
-                        if (item.Quality > 0)
-                        {
-                        
-                            item.Quality = item.Quality - 1;
-                        }
+
+                        item.Quality = item.Quality - 1;
                     }
                 }
-                if(!(item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert"))
+                if (!(item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert"))
                 {
                     if (item.Quality < 50)
                     {
